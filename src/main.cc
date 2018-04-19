f896712cb7a353418a1fe05170bd6ced5d0b419c
Juho Snellman <jsnell@iki.fi>
2018-04-19 03:27:52 +0200

    Gravity

--- src/main.cc
+++ src/main.cc
@@ -13,8 +13,8 @@ enum Direction {
 
 class Snake {
 public:
-    Snake(int r, int c)
-        : r_(r), c_(c) {
+    Snake(int id, int r, int c)
+        : id_(id), r_(r), c_(c) {
     }
 
     void grow(Direction dir) {
@@ -33,6 +33,7 @@ public:
         tail_.pop_back();
     }
 
+    int id_;
     int r_;
     int c_;
     std::deque<Direction> tail_;
@@ -71,6 +72,7 @@ public:
                 Direction dir((Direction) i);
                 if (is_valid_move(snake, dir)) {
                     if (fun(snake, dir)) {
+                        process_gravity();
                         return;
                     }
                 }
@@ -98,6 +100,68 @@ public:
         return false;
     }
 
+    void process_gravity() {
+        bool again;
+        do {
+            again = false;
+            draw_snakes();
+            for (auto snake : snakes_) {
+                if (!snake_is_supported(snake)) {
+                    snake->r_++;
+                    again = true;
+                }
+            }
+        } while (again);
+    }
+
+    // Consider snake S supported if there is at least one square
+    // that's non-empty on the base map or contains a segment of
+    // another snake, and which is directly beneath a segment of S.
+    //
+    // Note: this is incorrect, and just an expedient choice to get
+    // started with. Consider something like:
+    //
+    //     AA
+    //    BBA
+    //   . AA
+    //   .
+    //   ....
+    //
+    // Proper solution that requires two passes + a graph search:
+    //
+    // - First determine for all snakes if they are supported by
+    //   a) the base map, b) other snakes (and which ones?).
+    // - Then for each snake try to trace through the support
+    //   graph all the way to the ground.
+    bool snake_is_supported(Snake* snake) {
+        int r = snake->r_ + 1;
+        int c = snake->c_;
+
+        if ((snake_map_[r * c_ + c] != '\0' &&
+             snake_map_[r * c_ + c] != snake->id_) ||
+            (map_[r * c_ + c] != ' ')) {
+            return true;
+        }
+
+        for (auto dir : snake->tail_) {
+            switch (dir) {
+            case DOWN: --r; break;
+            case LEFT: ++c; break;
+            case UP: ++r; break;
+            case RIGHT: --c; break;
+            default: assert(false);
+            }
+
+            if ((snake_map_[r * c_ + c] != '\0' &&
+                 snake_map_[r * c_ + c] != snake->id_) ||
+                (map_[r * c_ + c] != ' ')) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
     void draw_snakes() {
         memset(snake_map_, 0, r_ * c_);
         for (auto snake : snakes_) {
@@ -109,7 +173,7 @@ public:
         int r = snake->r_;
         int c = snake->c_;
 
-        snake_map_[r * c_ + c] = 'a';
+        snake_map_[r * c_ + c] = snake->id_;
 
         for (auto dir : snake->tail_) {
             switch (dir) {
@@ -119,7 +183,7 @@ public:
             case RIGHT: --c; break;
             default: assert(false);
             }
-            snake_map_[r * c_ + c] = 'A';
+            snake_map_[r * c_ + c] = snake->id_;
         }
     }
 
@@ -135,13 +199,14 @@ const char* map =
     ".     ."
     ".     ."
     ".  .. ."
+    ".     ."
     ". *   ."
     ".......";
 
 int main() {
-    State state(map, 6, 7);
+    State state(map, 7, 7);
 
-    Snake a(2, 3);
+    Snake a('a', 2, 3);
     a.grow(RIGHT);
     a.grow(DOWN);
     state.add_snake(&a);
