0c809b895a4e67ed8615e3a9e271803ae3a56948
Juho Snellman <jsnell@iki.fi>
2018-04-19 02:42:14 +0200

    Iterate through valid moves

--- src/main.cc
+++ src/main.cc
@@ -3,10 +3,12 @@
 #include <cstdio>
 #include <cstring>
 #include <deque>
+#include <functional>
 #include <vector>
 
 enum Direction {
-    UP, RIGHT, DOWN, LEFT
+    UP, RIGHT, DOWN, LEFT,
+    MAX_DIR
 };
 
 class Snake {
@@ -25,6 +27,7 @@ public:
         case RIGHT: ++c_; break;
         case DOWN: ++r_; break;
         case LEFT: --c_; break;
+        default: assert(false);
         }
         tail_.push_front(dir);
         tail_.pop_back();
@@ -46,9 +49,6 @@ public:
     }
 
     void print() {
-        for (auto snake : snakes_) {
-            draw_snake(snake);
-        }
         for (int i = 0; i < r_; ++i) {
             for (int j = 0; j < c_; ++j) {
                 printf("%c",
@@ -61,13 +61,54 @@ public:
 
     void add_snake(Snake* snake) {
         snakes_.push_back(snake);
+        draw_snakes();
+    }
+
+    void do_valid_moves(std::function<bool(Snake*, Direction dir)> fun) {
+        draw_snakes();
+        for (auto snake : snakes_) {
+            for (int i = 0; i < MAX_DIR; ++i) {
+                Direction dir((Direction) i);
+                if (is_valid_move(snake, dir)) {
+                    if (fun(snake, dir)) {
+                        return;
+                    }
+                }
+            }
+        }
+        printf("No valid moves\n");
+    }
+
+    bool is_valid_move(Snake* snake, Direction dir) {
+        int r = snake->r_;
+        int c = snake->c_;
+
+        switch (dir) {
+        case UP: --r; break;
+        case RIGHT: ++c; break;
+        case DOWN: ++r; break;
+        case LEFT: --c; break;
+        default: assert(false);
+        }
+
+        if ((snake_map_[r * c_ + c] ^ map_[r * c_ + c]) == ' ') {
+            return true;
+        }
+
+        return false;
+    }
+
+    void draw_snakes() {
+        memset(snake_map_, 0, r_ * c_);
+        for (auto snake : snakes_) {
+            draw_snake(snake);
+        }
     }
 
     void draw_snake(Snake* snake) {
         int r = snake->r_;
         int c = snake->c_;
 
-        memset(snake_map_, 0, r_ * c_);
         snake_map_[r * c_ + c] = 'a';
 
         for (auto dir : snake->tail_) {
@@ -76,6 +117,7 @@ public:
             case LEFT: ++c; break;
             case UP: ++r; break;
             case RIGHT: --c; break;
+            default: assert(false);
             }
             snake_map_[r * c_ + c] = 'A';
         }
@@ -105,10 +147,18 @@ int main() {
     state.add_snake(&a);
 
     state.print();
-    a.move(RIGHT);
-    state.print();
-    a.move(RIGHT);
-    state.print();
+
+    for (int i = 0; i < 10; ++i) {
+        state.do_valid_moves([](Snake* snake, Direction dir) {
+                snake->move(dir);
+                return true;
+            });
+        state.print();
+    }
+    // a.move(RIGHT);
+    // state.print();
+    // a.move(RIGHT);
+    // state.print();
 
     return 0;
 }
